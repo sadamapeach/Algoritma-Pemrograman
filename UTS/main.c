5f856ac73f7b148f3dbe84b2e53fc55829582387
Oktaviana Sadama <100354765+sadamapeach@users.noreply.github.com>
2024-04-26 23:39:07 +0700

    Latihan UTS

---
+++ UTS/main.c
@@ -0,0 +1,25 @@
+/*#include <stdio.h>
+
+int main(){
+    // Kamus Lokal
+    int i,j,N;
+
+    printf("Masukkan N: ");
+    scanf("%d", &N);
+
+    for(i=1; i<=N; i++){
+        if(i%2==0){
+            if(i%4==0){
+                printf("%d\n",j+1);
+            }
+            else{
+                printf("%d\n",j+(2*i));
+            }
+        }
+        else{
+            printf("%d\n",j+(j*(i*i)));
+        }
+    }
+    return 0;
+}
+*/
